968d258c37b16e61a8feef9fe8e3907979e0d01d
Alejandro Zapata <alejo8708@gmail.com>
2023-04-11 12:51:17 +0200

    env-teak

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,15 @@
+//Dependencies
 const asyncHandler = require("express-async-handler")
+const bcrypt = require("bcryptjs")
+
+//Database
+const User = require("../db/models/userModel")
+
 
 //@route /api/users @Public
 const registerUser = asyncHandler( async (req, res) => {
     const {name,email,password} = req.body
+
 
     //Validation
     if(!name || !email || !password) {
@@ -10,7 +17,36 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new Error("Please include all fields")
     }
 
-    res.send("Register User")
+    console.log(name, email, password)
+    //Find if user already exists
+    const userExists = await User.findOne({email: email})
+    if(userExists) {
+        res.status(400)
+        throw new Error("User already exists")
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+
 })
 
 //@route /api/users/login @Public
--- envConfig.js
+++ envConfig.js
@@ -1,7 +1,7 @@
 const envConfig = {
-    run_mode: process.env.NODE_ENV ? "production" : "development",
+    run_mode: process.env.NODE_ENV === "production" ? "production" : "development",
     port: Number(process.env.PORT) || 8080,
-    db_url: process.env.CLOUD_DB || process.env.DB_URL
+    db_url: process.env.CLOUD_DB || process.env.DB_LOCAL_URL
 }
 
 module.exports = envConfig
