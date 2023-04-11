dc96ded3d1cb3039d769909d208b0fed15637e77
Alejandro Zapata <alejo8708@gmail.com>
2023-04-11 17:41:29 +0200

    react-vite-setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container flex justify-center text-blue-500 font-semibold text-4xl mx-auto w-1/3">Hello Alejandro</div>
+    </>
+  )
+}
+
+export default App
